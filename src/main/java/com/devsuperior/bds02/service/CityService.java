ccf08c979ac5481a1a5bc5b551e29020b1f2f167
Cainã Adrian <adrianmuniz.ceo@outlook.com>
2022-09-17 21:07:34 -0300

    CityService e FindAll

---
+++ src/main/java/com/devsuperior/bds02/service/CityService.java
@@ -0,0 +1,22 @@
+package com.devsuperior.bds02.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.repository.CityRepository;
+
+public class CityService {
+
+	@Autowired
+	private CityRepository repository;
+
+	public List<CityDTO> findAll() {
+		List<City> list = repository.findAll(Sort.by("name"));
+		return list.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
+	}
+}
